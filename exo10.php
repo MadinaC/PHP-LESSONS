13d68f3c47e8ceaab4111c68de257d2b099bc4e3
MadinaC <128142246+MadinaC@users.noreply.github.com>
2023-03-22 16:54:21 +0100

    Add files via upload

---
+++ exo10.php
@@ -0,0 +1,18 @@
+<h1>excercice10</h>
+
+<?php
+
+$totalinvoice = 152 ;
+$amountpaid = 200 ;
+$moneyback = $amountpaid - $totalinvoice ;
+$ten= 0;
+$five= 0;
+$two = 0;
+$one= 0;
+while ($moneyback >= )  {
+    $ten++;
+    $moneyback-=10;
+}
+
+
+
